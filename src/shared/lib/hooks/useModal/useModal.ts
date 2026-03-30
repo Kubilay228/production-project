3aa1a4580a4e5164d7fde6be463a9bd2d05906ff
Kubi <stepancrazy89@gmail.com>
2026-03-30 20:35:30 +0300

    new hook useModal

---
+++ src/shared/lib/hooks/useModal/useModal.ts
@@ -0,0 +1,61 @@
+import {
+    useEffect, useCallback, useState, MutableRefObject, useRef,
+} from 'react';
+
+interface UseModalProps {
+    onClose?: () => void
+    animationDelay?: number
+    isOpen?: boolean
+}
+
+export const useModal = (props: UseModalProps) => {
+    const {
+        onClose,
+        animationDelay,
+        isOpen,
+    } = props;
+
+    const [isClosing, setIsClosing] = useState(false);
+    const [isMounted, setIsMounted] = useState(false);
+    const timerRef = useRef() as MutableRefObject<ReturnType<typeof setTimeout>>;
+
+    useEffect(() => {
+        if (isOpen) {
+            setIsMounted(true);
+        }
+    }, [isOpen]);
+
+    const closeHandler = useCallback(() => {
+        if (onClose) {
+            setIsClosing(true);
+            timerRef.current = setTimeout(() => {
+                onClose();
+                setIsClosing(false);
+            }, animationDelay);
+        }
+    }, [animationDelay, onClose]);
+
+    // Новые ссылки!!!
+    const onKeyDown = useCallback((e: KeyboardEvent) => {
+        if (e.key === 'Escape') {
+            closeHandler();
+        }
+    }, [closeHandler]);
+
+    useEffect(() => {
+        if (isOpen) {
+            window.addEventListener('keydown', onKeyDown);
+        }
+
+        return () => {
+            clearTimeout(timerRef.current);
+            window.removeEventListener('keydown', onKeyDown);
+        };
+    }, [isOpen, onKeyDown]);
+
+    return {
+        isClosing,
+        isMounted,
+        closeHandler,
+    };
+};
