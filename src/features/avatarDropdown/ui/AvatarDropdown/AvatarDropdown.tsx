d97af87825795d543709412ca79865cd0e85e605
Kubi <stepancrazy89@gmail.com>
2026-03-30 19:37:39 +0300

    init

---
+++ src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.tsx
@@ -0,0 +1,55 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+import { useCallback } from 'react';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import { Avatar } from 'shared/ui/Avatar/Avatar';
+import { Dropdown } from 'shared/ui/Popups';
+import {
+    getUserAuthData, isUserAdmin, isUserManager, userActions,
+} from 'entities/User';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import cls from './AvatarDropdown.module.scss';
+
+interface AvatarDropdownProps {
+    className?: string;
+}
+
+export const AvatarDropdown = ({ className }: AvatarDropdownProps) => {
+    const { t } = useTranslation();
+    const dispatch = useAppDispatch();
+
+    const authData = useSelector(getUserAuthData);
+    const isAdmin = useSelector(isUserAdmin);
+    const isManager = useSelector(isUserManager);
+    const isAdminPanelAvailable = isAdmin || isManager;
+    const onLogout = useCallback(() => {
+        dispatch(userActions.logout());
+    }, [dispatch]);
+
+    if (!authData) {
+        return null;
+    }
+
+    return (
+        <Dropdown
+            direction="bottom left"
+            className={cls.dropdown}
+            items={[
+                ...(isAdminPanelAvailable ? [{
+                    content: t('Админка'),
+                    href: RoutePath.admin_panel,
+                }] : []),
+                {
+                    content: t('Профиль'),
+                    href: RoutePath.profile + authData.id,
+                },
+                {
+                    content: t('Выйти'),
+                    onClick: onLogout,
+                },
+            ]}
+            trigger={<Avatar size={30} src={authData.avatar} />}
+        />
+    );
+};
---
+++ src/shared/ui/Popups/index.ts
@@ -0,0 +1,3 @@
+export { Dropdown } from './components/Dropdown/Dropdown';
+export { Popover } from './components/Popover/Popover';
+export { ListBox } from './components/ListBox/ListBox';
